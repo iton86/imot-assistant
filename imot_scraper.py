82f1f3000184042b139b1047babac399cc075fb6
iton86 <iton86@abv.bg>
2023-10-04 20:51:15 +0300

    Added write_to_db method and some refactoring

--- imot_scraper.py
+++ imot_scraper.py
@@ -1,44 +1,43 @@
-import numpy as np
-import pandas as pd
+import io
+import hashlib
 import requests
 import importlib
-# from settings import *
+import numpy as np
+import pandas as pd
+import psycopg2 as pg
+from datetime import datetime
+from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+from urllib.parse import unquote, quote
+
 import settings as s
 importlib.reload(s)
 
-from bs4 import BeautifulSoup
-from urllib.parse import unquote, quote
 
 class ImotScraper:
 
     """
-    Scrapes the list of URLs
+    Scrapes the list of URLs and loads them in DB.
     """
 
-    def __init__(self, main_url, imot_cities, imot_regions, imot_types, payload, headers, ads_url_2, encoding):
+    def __init__(self):
+        """
         """
 
-        :param main_url:
-        :param imot_cities:
-        :param imot_regions:
-        :param imot_types:
-        :param payload:
-        :param headers:
-        :param imot_slinks:
-        """
-
-        self.main_url = main_url
-        self.imot_cities = imot_cities
-        self.imot_regions = imot_regions
-        self.imot_types = imot_types
-        self.payload = payload
-        self.headers = headers
-        self.encoding = encoding
-        self.ads_url_2 = ads_url_2
+        self.main_url = s.MAIN_URL
+        self.imot_cities = s.IMOT_CITIES
+        self.imot_regions = s.IMOT_REGIONS
+        self.imot_types = s.IMOT_TYPES
+        self.payload = s.PAYLOAD
+        self.headers = s.HEADERS
+        self.encoding = s.ENCODING
+        self.ads_url_2 = s.ADS_URL_2
+        self.pg_connection_string = s.PG_CONNECTION_STRING
 
         self.imot_slinks = {}
         self.all_ad_urls = []
         self.all_ads_details = pd.DataFrame()
+
 
     def get_slinks(self):
         """
@@ -124,6 +123,15 @@ class ImotScraper:
         :return:
         """
 
+        def hash_description(x):
+
+            hash_object = hashlib.sha256()
+            byte_data = x.encode()
+            hash_object.update(byte_data)
+            hashed_data = hash_object.hexdigest()
+
+            return hashed_data
+
         print(ad_url)
         ad_info = requests.get(ad_url)
         ad_info.encoding = self.encoding
@@ -184,6 +192,8 @@ class ImotScraper:
         except Exception as e:
             print(e)
 
+        str_to_hash = ad_url+ad_city+ad_neighborhood+ad_type+ad_description+str(ad_price)+\
+                      ad_currency+str(ad_kvm)+str(ad_price_per_kvm)+ad_street
         try:
             ad_details = pd.DataFrame({
                 # 'pic_url': pic_url,
@@ -192,12 +202,14 @@ class ImotScraper:
                 'ad_neighborhood': ad_neighborhood,
                 'ad_type': ad_type,
                 'ad_description': ad_description,
-                'ad_price': ad_price,
+                'ad_descr_hash': hash_description(str_to_hash),
+                'ad_price': int(ad_price),
                 'ad_currency': ad_currency,
-                'ad_kvm': ad_kvm,
+                'ad_kvm': int(ad_kvm),
                 'ad_price_per_kvm': ad_price_per_kvm,
                 # 'ad_price_change': ad_price_change,
-                'ad_street': ad_street}, index=[0])
+                'ad_street': ad_street,
+                'updated_dt': datetime.today().strftime('%Y-%m-%d %H:%M:%S')}, index=[0])
 
             return ad_details
 
@@ -212,20 +224,91 @@ class ImotScraper:
         for ad_url in self.all_ad_urls:
             self.all_ads_details = pd.concat([self.all_ads_details, self.get_ad_info(ad_url)])
 
-        self.all_ads_details['avg_price_per_kvm_of_sample'] = self.all_ads_details.groupby(
-            ['ad_city',
-             'ad_neighborhood',
-             'ad_type'])['ad_price_per_kvm'].transform('mean')
+        # self.all_ads_details['avg_price_per_kvm_of_sample'] = self.all_ads_details.groupby(
+        #     ['ad_city',
+        #      'ad_neighborhood',
+        #      'ad_type'])['ad_price_per_kvm'].transform('mean')
+        #
+        # self.all_ads_details['price_per_kvm_gain'] = \
+        #     self.all_ads_details['ad_price_per_kvm'] / self.all_ads_details['avg_price_per_kvm_of_sample'] - 1
+        # self.all_ads_details['price_per_kvm_gain'] = round(self.all_ads_details['price_per_kvm_gain'] * 100, 2)
 
-        self.all_ads_details['price_per_kvm_gain'] = \
-            self.all_ads_details['ad_price_per_kvm'] / self.all_ads_details['avg_price_per_kvm_of_sample'] - 1
-        self.all_ads_details['price_per_kvm_gain'] = round(self.all_ads_details['price_per_kvm_gain'] * 100, 2)
+    def write_to_db(self, table_name_latest, table_name_history):
+
+        engine = create_engine(self.pg_connection_string)
+        conn = engine.raw_connection()
+        # cur = conn.cursor()
+
+        # table_name_latest = 'ads_latest'
+        # table_name_history = 'ads_history'
+        with conn.cursor() as cur:
+
+            cur.execute(
+                f"""
+                CREATE TEMPORARY TABLE tmp (LIKE {table_name_history})
+                """
+            )
+
+            output = io.StringIO()
+            self.all_ads_details.to_csv(output, sep='\t', header=False, index=False)
+            # imot.all_ads_details.to_csv(output, sep='\t', header=False, index=False)
+            output.seek(0)
 
 
-imot = ImotScraper(s.MAIN_URL, s.IMOT_CITIES, s.IMOT_REGIONS, s.IMOT_TYPES,
-                   s.PAYLOAD, s.HEADERS, s.ADS_URL_2, s.ENCODING)
+            cur.copy_from(output, 'tmp')
+
+            cur.execute(
+                f"""
+                INSERT INTO {table_name_latest}
+                SELECT * FROM tmp
+                ON CONFLICT (ad_url) DO UPDATE
+                    SET ad_city = CASE WHEN {table_name_latest}.ad_city <> excluded.ad_city THEN excluded.ad_city ELSE {table_name_latest}.ad_city END,
+                        ad_neighborhood = CASE WHEN {table_name_latest}.ad_neighborhood <> excluded.ad_neighborhood THEN excluded.ad_neighborhood ELSE {table_name_latest}.ad_neighborhood END,
+                        ad_type = CASE WHEN {table_name_latest}.ad_type <> excluded.ad_type THEN excluded.ad_type ELSE {table_name_latest}.ad_type END,
+                        ad_description = CASE WHEN {table_name_latest}.ad_description <> excluded.ad_description THEN excluded.ad_description ELSE {table_name_latest}.ad_description END,
+                        ad_hash = CASE WHEN {table_name_latest}.ad_hash <> excluded.ad_hash THEN excluded.ad_hash ELSE {table_name_latest}.ad_hash END,
+                        ad_price = CASE WHEN {table_name_latest}.ad_price <> excluded.ad_price THEN excluded.ad_price ELSE {table_name_latest}.ad_price END,
+                        ad_currency = CASE WHEN {table_name_latest}.ad_currency <> excluded.ad_currency THEN excluded.ad_currency ELSE {table_name_latest}.ad_currency END,
+                        ad_kvm = CASE WHEN {table_name_latest}.ad_kvm <> excluded.ad_kvm THEN excluded.ad_kvm ELSE {table_name_latest}.ad_kvm END,
+                        ad_price_per_kvm = CASE WHEN {table_name_latest}.ad_price_per_kvm <> excluded.ad_price_per_kvm THEN excluded.ad_price_per_kvm ELSE {table_name_latest}.ad_price_per_kvm END,
+                        ad_street = CASE WHEN {table_name_latest}.ad_street <> excluded.ad_street THEN excluded.ad_street ELSE {table_name_latest}.ad_street END
+                        -- updated_ts = CASE WHEN {table_name_latest}.updated_ts <> excluded.updated_ts THEN excluded.updated_ts ELSE {table_name_latest}.updated_ts END
+                    
+                RETURNING *;
+                """
+            )
+
+            cur.execute(
+                f"""
+                INSERT INTO {table_name_history}
+                SELECT * FROM tmp
+                ON CONFLICT (ad_hash) 
+                DO NOTHING
+                """
+            )
+
+            cur.execute(f"DROP TABLE tmp")
+            conn.commit()
+        conn.close()
+
+
+imot = ImotScraper()
 imot.get_slinks()
 print(imot.imot_slinks)
 imot.get_all_ads()
 imot.get_all_ads_info()
-r = imot.all_ads_details.reset_index(drop=True)
+imot.write_to_db('ads_latest', 'ads_history')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
