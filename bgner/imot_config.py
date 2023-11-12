a93307e8b1a8759df645fe021c118c41cca8c3eb
iton86 <iton86@abv.bg>
2023-11-12 17:51:54 +0200

    Trying to add bgner dir

---
+++ bgner/imot_config.py
@@ -0,0 +1,24 @@
+# Model parameters
+
+MODEL_PATH = 'bgner/model/imot_ner_model_2023-11-10'
+TOKENIZER_PATH = 'bgner/model/imot_ner_tokenizer_2023-11-10'
+# MODEL_PATH = 'bert-base-multilingual-cased'
+# TOKENIZER_PATH = 'bert-base-multilingual-cased'
+ENCODING = 'windows-1251'
+
+TAG_TO_ID = {
+    'O': 0,
+    'B-FAC': 1,
+    'I-FAC': 2
+}
+
+TRAINING_PARAMS = {
+    'base_model': 'bert-base-multilingual-cased',
+    'max_len': 128,
+    'batch_size': 16,
+    'epochs': 5,
+    'learning_rate': 1e-5,
+    'test_sample_size': 0.2,
+    'val_sample_size': 0.5,  # From test_sample_size
+    'random_state': 171
+}
